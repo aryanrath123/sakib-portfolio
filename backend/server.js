0bb758d1bab5cc03742bb74c560e17a9ec51b584
aryanrath123 <rath2004aryan@gmail.com>
2025-05-31 08:30:40 +0530

    Fix path-to-regexp error by downgrading Express

--- backend/server.js
+++ backend/server.js
@@ -80,7 +80,7 @@ app.post("/ask", async (req, res) => {
 });
 
 // Serve frontend for all other routes
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
